d9a9f4a28e8a8097c4ae0f547b38143346ff44eb
sarayu1310 <sarayugosika2004@gmail.com>
2025-08-21 12:29:46 +0530

    added port

--- server.js
+++ server.js
@@ -104,4 +104,5 @@ io.on("connection", (socket) => {
       });
     });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(5000, () => console.log(`Server running on port ${PORT}`));
