0deb5fcc6a343e776322f53cdd546ec98388a226
sarayu1310 <sarayugosika2004@gmail.com>
2025-08-21 14:45:23 +0530

    edited origin

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000"], 
+      origin: ["http://localhost:3000", "https://whiteboard-theta-three.vercel.app"], 
       methods: ["GET", "POST"],
     },
   });
